d29f82842def6a3b7d6e2d6657342cea127193fb
Allnorm <ruallnorm@gmail.com>
2021-06-15 17:27:29 +0300

    Fixed a bug when a bot responded to commands sent to other bots.

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ from qwerty import qwerty_main
 from translate import translate_main
 from inline import query_text_main
 
-logger.write_log("###POLYGLOT v0.4 beta build 1 HAS BEEN STARTED###")
+logger.write_log("###POLYGLOT v0.4 beta build 2 HAS BEEN STARTED###")
 utils.list_of_langs()
+
+
+def botname_checker(message):  # Crutch to prevent the bot from responding to other bots commands
+    if ("@" in message.text and "@" + utils.bot.get_me().username in message.text) or not ("@" in message.text):
+        return True
+    else:
+        return False
 
 
 @utils.bot.inline_handler(lambda query: len(query.query) >= 0)
@@ -22,79 +29,95 @@ def query_text(inline_query):
 @utils.bot.message_handler(commands=['qwerty', 'q'])
 def qwerty(message):
 
-    qwerty_main(message)
+    if botname_checker(message):
+        qwerty_main(message)
 
 
 @utils.bot.message_handler(commands=['d', 'distort'])
 def distort(message):
-    thread = threading.Thread(target=distort_main, args=(message,))
-    thread.start()
+
+    if botname_checker(message):
+        thread = threading.Thread(target=distort_main, args=(message,))
+        thread.start()
 
 
 @utils.bot.message_handler(commands=['translate', 'trans', 't'])
 def translate(message):
-    translate_main(message)
+
+    if botname_checker(message):
+        translate_main(message)
 
 
 @utils.bot.message_handler(commands=['start'])
 def send_welcome(message):
-    logger.write_log(logger.BLOB_TEXT, message)
-    utils.bot.reply_to(message, "Привет. Я бот - переводчик. "
-                                "Работаю на основе Google Translate API, и могу переводить сообщения "
-                                "в чате на лету.\n\n"
-                                "Для этого добавь меня в чат, и при необходимости перевести чьё-то "
-                                "сообщение 'Ответь' на него, и напиши команду: /t <код языка>. "
-                                "Исходный язык перевода бот определит автоматически.\n\n"
-                                "Остальные команды можно узнать командой /help.\n\n"
-                                "Также я могу работать в личных сообщениях, как обычный переводчик.")
+
+    if botname_checker(message):
+        logger.write_log(logger.BLOB_TEXT, message)
+        utils.bot.reply_to(message, "Привет. Я бот - переводчик. "
+                                    "Работаю на основе Google Translate API, и могу переводить сообщения "
+                                    "в чате на лету.\n\n"
+                                    "Для этого добавь меня в чат, и при необходимости перевести чьё-то "
+                                    "сообщение 'Ответь' на него, и напиши команду: /t <код языка>. "
+                                    "Исходный язык перевода бот определит автоматически.\n\n"
+                                    "Остальные команды можно узнать командой /help.\n\n"
+                                    "Также я могу работать в личных сообщениях, как обычный переводчик.")
 
 
 @utils.bot.message_handler(commands=['help', 'h'])
 def send_help(message):
-    logger.write_log(logger.BLOB_TEXT, message)
-    utils.bot.reply_to(message, "[/t, /trans, /translate] <язык> - перевести сообщение. Исходный язык определяется "
-                                "автоматически. Коды языков можно узнать с помощью команды /langs или /l\n"
-                                "[/l, /langs] - список доступных языковых кодов и раскладок клавиатуры\n"
-                                "[/d, /distort] <количество итераций> <итоговый язык> - "
-                                "Перевести сообщение на заданное количество "
-                                "рандомных языков и вывести результат на нужном вам языке. "
-                                "Если оставить параметр <итоговый язык> пустым, "
-                                "результат будет выведен на языке оригинала\n"
-                                "[/q, /qwerty] <исходный язык> <итоговый язык> ИЛИ "
-                                "/q <итоговый язык> - смена раскладки текста. Исходный язык может определяться "
-                                "автоматически. Список доступных раскладок можно посмотреть с помощью команды /langs")
+
+    if botname_checker(message):
+        logger.write_log(logger.BLOB_TEXT, message)
+        utils.bot.reply_to(message, "[/t, /trans, /translate] <язык> - перевести сообщение. Исходный язык определяется "
+                                    "автоматически. Коды языков можно узнать с помощью команды /langs или /l\n"
+                                    "[/l, /langs] - список доступных языковых кодов и раскладок клавиатуры\n"
+                                    "[/d, /distort] <количество итераций> <итоговый язык> - "
+                                    "Перевести сообщение на заданное количество "
+                                    "рандомных языков и вывести результат на нужном вам языке. "
+                                    "Если оставить параметр <итоговый язык> пустым, "
+                                    "результат будет выведен на языке оригинала\n"
+                                    "[/q, /qwerty] <исходный язык> <итоговый язык> ИЛИ "
+                                    "/q <итоговый язык> - смена раскладки текста. Исходный язык может определяться "
+                                    "автоматически. Список доступных раскладок можно посмотреть с помощью команды /langs")
 
 
 @utils.bot.message_handler(commands=['langs', 'l'])
 def send_list(message):
-    logger.write_log(logger.BLOB_TEXT, message)
 
-    try:
-        file = open("langlist.txt", "r")
-        utils.bot.send_document(message.chat.id, file, message.id, "Здесь список всех языков для перевода и раскладок")
-    except FileNotFoundError:
-        logger.write_log("WARN: Trying to re-create removed langlist file")
-        utils.bot.reply_to(message, "Ошибка, список языков отсутствует. Попытка пересоздания файла, попробуйте "
-                                    "отправить команду ещё раз. "
-                                    "Если это не сработает, обратитесь к авторам бота.")
-        utils.list_of_langs()
-    except Exception as e:
-        logger.write_log("ERR: langlist isn't available")
-        logger.write_log("ERR: " + str(e) + "\n" + traceback.format_exc())
-        utils.bot.reply_to(message, "Ошибка чтения файла с языками Обратитесь к авторам бота\n"
-                                    "Информация для отладки сохранена в логах бота.")
+    if botname_checker(message):
+        logger.write_log(logger.BLOB_TEXT, message)
+
+        try:
+            file = open("langlist.txt", "r")
+            utils.bot.send_document(message.chat.id, file, message.id,
+                                    "Здесь список всех языков для перевода и раскладок")
+        except FileNotFoundError:
+            logger.write_log("WARN: Trying to re-create removed langlist file")
+            utils.bot.reply_to(message, "Ошибка, список языков отсутствует. Попытка пересоздания файла, попробуйте "
+                                        "отправить команду ещё раз. "
+                                        "Если это не сработает, обратитесь к авторам бота.")
+            utils.list_of_langs()
+        except Exception as e:
+            logger.write_log("ERR: langlist isn't available")
+            logger.write_log("ERR: " + str(e) + "\n" + traceback.format_exc())
+            utils.bot.reply_to(message, "Ошибка чтения файла с языками Обратитесь к авторам бота\n"
+                                        "Информация для отладки сохранена в логах бота.")
 
 
 @utils.bot.message_handler(commands=['downloadlog'])
 def download_log(message):
-    logger.write_log(logger.BLOB_TEXT, message)
-    logger.download_clear_log(message, True)
+
+    if botname_checker(message):
+        logger.write_log(logger.BLOB_TEXT, message)
+        logger.download_clear_log(message, True)
 
 
 @utils.bot.message_handler(commands=['clearlog'])
 def clear_log(message):
-    logger.write_log(logger.BLOB_TEXT, message)
-    logger.download_clear_log(message, False)
+
+    if botname_checker(message):
+        logger.write_log(logger.BLOB_TEXT, message)
+        logger.download_clear_log(message, False)
 
 
 utils.bot.infinity_polling(True)
