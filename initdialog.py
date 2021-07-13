7d77c604544d1c2ee48ecf0e74da43ddf26b6b43
Allnorm <les.pesosnie.shasi.310501@gmail.com>
2021-07-14 00:03:34 +0300

    Changed initdialog algorithm, version number upped to 0.6

--- initdialog.py
+++ initdialog.py
@@ -1,3 +1,4 @@
+import configparser
 import sys
 import traceback
 
@@ -12,13 +13,16 @@ def init_dialog():
     keypath = input("Please, write path to your JSON Google API Key (optional, key.json as default): ")
     if keypath == "":
         keypath = "key.json"
+
+    config = configparser.ConfigParser()
+    config.add_section("Polyglot")
+    config.set("Polyglot", "token", token)
+    config.set("Polyglot", "key", key)
+    config.set("Polyglot", "keypath", keypath)
+    config.set("Polyglot", "max-inits", "100")
+    # This is an default configuration of Polyglot bot
     try:
-        file = open("polyglot.ini", "w")
-        file.write("[Polyglot]\n"
-                    "token=" + token + "\n"
-                    "key=" + key + "\n"
-                    "keypath=" + keypath + "\n"
-                    "max-inits=10")  # This is an default configuration of Polyglot bot
+        config.write(open("polyglot.ini", "w"))
         logger.write_log("INFO: New config file was created successful")
     except IOError as e:
         logger.write_log("ERR: Bot cannot write new config file and will close")
