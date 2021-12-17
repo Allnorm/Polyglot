9e390a01c6d1176a52ec88a585bc23e47b1d514a
Allnorm <ruallnorm@gmail.com>
2021-12-17 09:46:07 +0300

    Removed update hook from sql worker

--- sql_worker.py
+++ sql_worker.py
@@ -29,12 +29,6 @@ def table_init():
                                     region TEXT NOT NULL,
                                     expire_time INTEGER,
                                     chat_id TEXT NOT NULL);''')
-        try:  # Hook for upgrade from Polyglot 0.7-1.0
-            cursor.execute('''ALTER TABLE chats ADD COLUMN user_id TEXT''')
-            cursor.execute('''ALTER TABLE chats ADD COLUMN target_lang TEXT''')
-            logger.write_log("INFO: Triggered hook for upgrade from Polyglot 0.7-1.0")
-        except (sqlite3.OperationalError, sqlite3.DatabaseError):
-            pass
         sqlite_connection.commit()
     except (sqlite3.OperationalError, sqlite3.DatabaseError) as e:
         logger.write_log("ERR: write mySQL DB failed!")
