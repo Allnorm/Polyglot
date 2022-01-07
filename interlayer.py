c743164111b2fa9ebeb8aa74ade5988b271dcb4d
Allnorm <les.pesosnie.shasi.310501@gmail.com>
2022-01-07 18:32:59 +0300

    The interplayer is separated into an external file

--- interlayer.py
+++ interlayer.py
@@ -1,14 +1,5 @@
-import json
-import logging
-import os
-import sys
-import traceback
+import importlib
 
-from google.cloud import translate
-
-json_key = ""
-project_name = ""
-translator: translate.TranslationServiceClient
 lang_list = {}
 
 
@@ -36,87 +27,38 @@ class UnkTransException(Exception):
     pass
 
 
+trans_api = importlib.import_module("trans_api")
+
+
 def init_dialog_api(config):
-    keypath = input("Please, write path to your JSON Google API Key (optional, key.json as default): ")
-    if keypath == "":
-        keypath = "key.json"
-    config.set("Polyglot", "keypath", keypath)
-    return config
+
+    return trans_api.init_dialog_api(config)
 
 
 def api_init(config):
-    global project_name, json_key
 
-    version = "1.0.1 for googleapi 3.6.1"
-    build = "1"
-    version_polyglot = "1.2 alpha/beta/release"
-    build_polyglot = "- any"
-    logging.info("Interlayer version {}, build {}".format(version, build))
-    logging.info("Compatible with version of Polyglot {}, build {}".format(version_polyglot, build_polyglot))
-
-    try:
-        json_key = config["Polyglot"]["keypath"]
-    except KeyError:
-        raise
-
-    if not os.path.isfile(json_key):
-        logging.error("JSON file wasn't found! Bot will close!")
-        sys.exit(1)
-
-    try:
-        project_name = "projects/" + json.load(open(json_key, 'r')).get("project_id")
-    except Exception as e:
-        logging.error("Project name isn't readable from JSON! Bot will close!")
-        logging.error(str(e) + "\n" + traceback.format_exc())
-        sys.exit(1)
-
-    return config
+    return trans_api.api_init(config)
 
 
 def translate_init():
-    global translator
-    try:
-        translator = translate.TranslationServiceClient.from_service_account_json(json_key)
-    except Exception as e:
-        logging.error("Translator object wasn't created successful! Bot will close! "
-                      "Please check your JSON key or Google Cloud settings.")
-        logging.error(str(e) + "\n" + traceback.format_exc())
-        sys.exit(1)
+
+    trans_api.translate_init()
 
 
 def extract_lang(text):
-    try:
-        return translator.detect_language(parent=project_name, content=text, timeout=10).languages[0].language_code
-    except Exception as e:
-        logging.error(str(e) + "\n" + traceback.format_exc())
-        raise UnkTransException
+
+    return trans_api.extract_lang(text)
 
 
 def list_of_langs():
     global lang_list
-    lang_buffer = translator.get_supported_languages(parent=project_name, display_language_code="en", timeout=10)
-    for lang in lang_buffer.languages:
-        lang_list.update({lang.language_code: lang.display_name})
+    trans_api.list_of_langs()
+    lang_list = trans_api.lang_list
 
 
 def get_translate(input_text: str, target_lang: str, distorting=False, src_lang=None):
+
     try:
-        trans_result = translator.translate_text(parent=project_name, contents=[input_text],
-                                                 target_language_code=target_lang, source_language_code=src_lang,
-                                                 mime_type="text/plain", timeout=10).translations[0].translated_text
-    except Exception as e:
-        if "400 Target language is invalid." in str(e):
-            raise BadTrgLangException
-        if "400 Target language can't be equal to source language." in str(e):
-            raise EqualLangsException
-        if "400 Source language is invalid." in str(e):
-            raise BadSrcLangException
-        else:
-            logging.error(str(e) + "\n" + traceback.format_exc())
-            raise UnkTransException
-
-    if len(trans_result) > 4096 and distorting is False:
-        logging.warning("too long message for sending.")
-        raise TooLongMsg
-
-    return trans_result
+        return trans_api.get_translate(input_text, target_lang, distorting, src_lang)
+    except:
+        raise
