f23c4a6e96bffa2b6088d535c8ef1357d0ae1ad0
Allnorm <ruallnorm@gmail.com>
2022-06-11 13:03:22 +0300

    Added requirements

--- start.bat
+++ start.bat
@@ -1,9 +1,18 @@
 @echo off
+set interlayer=googlefreeapi
 title = Polyglot
-if not exist "src\interlayer" (
+if not exist src\interlayer (
     echo Interlayer not installed. Please check if the installation is correct.
 	pause
 	exit
 )
+if not exist first_launch (
+	echo Installing requirements, please wait...
+	pip install -r requirements.txt
+	cd src\interlayer
+	pip install -r %interlayer%.txt
+	cd ..\..\
+	type nul > first_launch
+)
 cd src
-python main.py googlefreeapi
+python main.py %interlayer%
